2dde8dbc8f8536b31d397ad770c09f82d77ca6ac
springmarker <springmarker@163.com>
2017-11-26 11:41:03 +0800

    kotlin测试

---
+++ src/main/java/freemusic/music/pojo/wy/Ar.kt
@@ -0,0 +1,3 @@
+package freemusic.music.pojo.wy
+
+data class Ar(var id: Int = 0, var name: String = "")
---
+++ src/main/java/freemusic/music/pojo/wy/H.kt
@@ -0,0 +1,7 @@
+package freemusic.music.pojo.wy
+
+data class H(internal var br: Int = 0,
+             var fid: Int = 0,
+             var size: Int = 0,
+             var vd: Double = 0.toDouble()
+)
